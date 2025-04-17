71b6f88ecbb2cf6eab6b2bc3f5577cd4cc626a44
papa-multi <papamulti29@gmail.com>
2025-04-17 04:29:47 +0330

    Delete src directory

--- src/core/network.js
+++
@@ -1,8 +0,0 @@
-class Network {
-  static CHAINID = 11155111;
-  static RPCURL = "https://ethereum-sepolia-rpc.publicnode.com";
-  static EXPLORER = "https://sepolia.etherscan.io/";
-  static SYMBOL = "ETH";
-}
-
-export default Network;
